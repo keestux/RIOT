7b14710f377a9d6f2f17f6f9177b871d7dfaa984
Kees Bakker <kees@ijzerbout.nl>
2019-09-19 23:16:35 +0200

    boards/sam0: refactor ADC_0 into plain ADC
    This change is for all boards with a sam0 cpu. This cpu just has one ADC.
    It is unnecessary to have defines with ADC_0_ prefix as if multiple ADCs
    are possible.
    
    Some defines were not used, such as ADC_0_EN, ADC_0_CHANNELS,
    ADC_MAX_CHANNELS, ADC_0_CLK_SOURCE, ADC_0_CHANNELS
    
    Change all ADC_0_ prefixes to ADC_

--- boards/common/saml1x/include/periph_conf.h
+++ boards/common/saml1x/include/periph_conf.h
@@ -143,17 +143,16 @@ static const i2c_conf_t i2c_config[] = {
  */
 #define ADC_NUMOF                          (1U)
 
-/* ADC 0 Default values */
-#define ADC_0_CLK_SOURCE                   0 /* GCLK_GENERATOR_0 */
-#define ADC_0_PRESCALER                    ADC_CTRLB_PRESCALER_DIV256
+/* ADC Default values */
+#define ADC_PRESCALER                      ADC_CTRLB_PRESCALER_DIV256
 
 static const adc_conf_chan_t adc_channels[] = {
     /* port, pin, muxpos */
     {GPIO_PIN(PA, 10), ADC_INPUTCTRL_MUXPOS(ADC_INPUTCTRL_MUXPOS_AIN8)},
 };
 
-#define ADC_0_NEG_INPUT                    ADC_INPUTCTRL_MUXNEG(0x18u)
-#define ADC_0_REF_DEFAULT                  ADC_REFCTRL_REFSEL_INTVCC2
+#define ADC_NEG_INPUT                      ADC_INPUTCTRL_MUXNEG(0x18u)
+#define ADC_REF_DEFAULT                    ADC_REFCTRL_REFSEL_INTVCC2
 /** @} */
 
 #ifdef __cplusplus
